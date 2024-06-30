cec8caf22ca2555cc27be786e1c14fd9c3c2a487
Kaupenjoe <N.Kaupenjohann@gmail.com>
2024-06-30 13:46:22 +0200

    Initial commit

---
+++ src/main/java/net/kaupenjoe/mccourse/util/ModTags.java
@@ -0,0 +1,27 @@
+package net.kaupenjoe.mccourse.util;
+
+import net.kaupenjoe.mccourse.MCCourseMod;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+
+public class ModTags {
+    public static class Blocks {
+        public static final TagKey<Block> NEEDS_FLUORITE_TOOL = createTag("needs_fluorite_tool");
+        public static final TagKey<Block> INCORRECT_FOR_FLUORITE_TOOL = createTag("incorrect_for_fluorite_tool");
+
+        private static TagKey<Block> createTag(String name) {
+            return TagKey.of(RegistryKeys.BLOCK, Identifier.of(MCCourseMod.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+        public static final TagKey<Item> TRANSFORMABLE_ITEMS = createTag("transformable_items");
+
+        private static TagKey<Item> createTag(String name) {
+            return TagKey.of(RegistryKeys.ITEM, Identifier.of(MCCourseMod.MOD_ID, name));
+        }
+    }
+}
